00eed598116564542959a98e30348f1dd7b78043
Arindam Gahlot <112582091+ArindamGahlot@users.noreply.github.com>
2022-10-31 09:46:54 -0500

    Added the login functionality to the login screen in homepage.

--- Front-End/HomePage.js
+++ Front-End/HomePage.js
@@ -1,6 +1,6 @@
 function showPassword() {
     var user_password = document.getElementById("password_user");
-    if (user_password.type === "password") {
+    if (user_password.type == "password") {
         user_password.type = "text";
     } 
     else {
@@ -8,3 +8,18 @@ function showPassword() {
     }
   }
   
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Manager" && password === "12345") {
+        alert("You have successfully logged in.");
+        location.replace("managergui.html");
+    } else {
+        alert("You have either wrong username or wrong password.");
+    }
+})
