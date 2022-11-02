9ce3e0468900b49dc5d092f3f69836f23f9d3ba0
Andrew Zehrer <andrewzehrer@tamu.edu>
2022-11-01 19:07:16 -0500

    Started work on ManagerGUI, and style updates

--- Front-End/HomePage.js
+++ Front-End/HomePage.js
@@ -2,12 +2,11 @@ function showPassword() {
     var user_password = document.getElementById("password_user");
     if (user_password.type == "password") {
         user_password.type = "text";
-    } 
-    else {
+    } else {
         user_password.type = "password";
     }
-  }
-  
+}
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 
@@ -16,10 +15,12 @@ loginButton.addEventListener("click", (event) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
+    // TODO: Add database call here
     if (username === "Manager" && password === "12345") {
-        alert("You have successfully logged in.");
+        alert("You have successfully logged in."); // Do we need this?
         location.replace("managergui.html");
     } else {
-        alert("You have either wrong username or wrong password.");
+        alert("Error: Username or Password is incorrect.");
     }
-})
+
+});
--- Front-End/managergui.js
+++ Front-End/managergui.js
@@ -1,1 +1,27 @@
-// This would have javasciprt for your gui.
+function logOut() {
+    var url = "HomePage.html";
+    var win = window.open(url, "_self");
+    win.focus();
+}
+
+function getManagerName() {
+    //TODO: Get the manager username that they inputted when they logged in
+    var name = "Manager";
+    document.getElementById("MgrNameLabel").innerHTML = name;
+}
+
+function openReports() {
+    // window.open("Reports.html", "_self");
+}
+
+function openInventory() {
+    // window.open("Inventory.html", "_self");
+}
+
+function openEmployees() {
+    // window.open("Employees.html", "_self");
+}
+
+function openNewProductMaker() {
+    // window.open("NewProductMaker.html", "_self");
+}
