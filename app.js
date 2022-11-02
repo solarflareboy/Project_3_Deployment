5a15343dbcc56af7237698b65d08481b8a96a48f
Chris <batchprogrammer314@gmail.com>
2022-11-02 01:39:54 -0500

    Added routes for different webpages, added "dev" npm script for devleopment

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+const express = require("express")
+const app = express()
+const port = 8000 | process.env.PORT
+
+app.use("/", express.static("./public/homepage"))
+app.use("/cashier", express.static("./public/cashier"))
+app.use("/customer", express.static("./public/customer"))
+app.use("/manager", express.static("./public/manager"))
+
+app.listen(port, () => {
+    console.log(`The server has started on port ${port}.`)
+})
