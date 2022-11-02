fe06a084a21dec8c0d5dbdd339fc9d2a63ba84b6
Chris <batchprogrammer314@gmail.com>
2022-11-02 08:44:48 -0500

    comments for app.js

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express")
 const app = express()
 const port = 8000 | process.env.PORT
 
+// The routing for URLs is done here
 app.use("/", express.static("./public/homepage"))
 app.use("/cashier", express.static("./public/cashier"))
 app.use("/manager", express.static("./public/manager"))
+
+// This allows other subfolders such as "media" to be shared
 app.use(express.static("./public"))
 
+// Starting the server
 app.listen(port, () => {
     console.log(`The server has started on port ${port}.`)
 })
